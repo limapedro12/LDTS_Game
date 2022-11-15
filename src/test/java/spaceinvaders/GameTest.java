e43defeec8160c256575172bcc139bc04ecb1abb
Pedro Januário <up202108768@edu.fe.up.pt>
2022-11-15 10:21:37 +0000

    Still trying to use pitest.

--- src/test/java/spaceinvaders/GameTest.java
+++ src/test/java/spaceinvaders/GameTest.java
@@ -1,12 +1,11 @@
 package spaceinvaders;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
     @Test
     public void f1(){
-        assertEquals(1,1);
+        Assertions.assertEquals(1,1);
     }
 }
