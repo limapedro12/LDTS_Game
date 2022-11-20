13ebfdc9cc7b1e3d0f63860bb8406e558d7c5732
up202108806 <up202108806@edu.fe.up.pt>
2022-11-20 11:20:34 +0000

    Creating terminal and reading keys

--- src/main/java/spaceinvaders/Application.java
+++ src/main/java/spaceinvaders/Application.java
@@ -1,7 +1,17 @@
 package spaceinvaders;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
     }
 }
--- src/main/java/spaceinvaders/Game.java
+++ src/main/java/spaceinvaders/Game.java
@@ -1,4 +1,52 @@
 package spaceinvaders;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 public class Game {
+    private int x = 10;
+    private int y = 10;
+    private Screen screen;
+    public Game() {
+        try {
+            Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(100, 50)).createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+    public void run() throws IOException {
+        while(true) {
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+            if (key.getKeyType() == KeyType.EOF) break;
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.stopScreen();
+        }
+    }
+
+    private void processKey(com.googlecode.lanterna.input.KeyStroke key) {
+        System.out.println(key);
+    }
 }
