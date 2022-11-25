3e7ba821f74dd048266bf98fb6721126dfa05e61
Tomas Teixeira <j.tomas.teixeira@gmail.com>
2022-11-25 11:44:38 +0000

    Game Test

--- src/test/java/spaceinvaders/GameTest.java
+++ src/test/java/spaceinvaders/GameTest.java
@@ -1,11 +1,36 @@
 package spaceinvaders;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
 
 public class GameTest {
-    @Test
-    public void f1(){
-        Assertions.assertEquals(1,1);
+
+    private Screen screen;
+    private Arena arena;
+
+    private Game game;
+    @BeforeEach
+    public void helper(){
+       game = new Game();
     }
+
+    @Test
+    private void draw() throws IOException {
+        screen.clear();
+       arena.draw(screen.newTextGraphics(), screen);
+        screen.refresh();
+        Mockito.verify(arena,Mockito.times(1)).draw(screen.newTextGraphics(), screen);
+    }
+
+
 }
