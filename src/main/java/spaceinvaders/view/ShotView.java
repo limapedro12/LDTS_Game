c8acf5b1bf8e55f9537d21bbfec9393a2c1b81e4
up202108738 <up202108738@up.pt>
2022-11-29 10:24:44 +0000

    Added AlienModel and AlienView

--- src/main/java/spaceinvaders/view/ShotView.java
+++ src/main/java/spaceinvaders/view/ShotView.java
@@ -7,6 +7,8 @@ import spaceinvaders.model.*;
 public class ShotView implements ElementView {
     ShotModel model;
 
+
+
     public void draw(TextGraphics graphics) {
         graphics.setCharacter(model.getPosition().getX(), model.getPosition().getY(), TextCharacter.fromCharacter(model.getCharacter())[0]);
     }
