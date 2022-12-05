f035429dcf766a1e22bfc5fe593d5ccec19dc830
up202108738 <up202108738@up.pt>
2022-12-05 11:31:37 +0000

    Alien test e alienshot test

--- src/test/java/spaceinvaders/AlienTest.java
+++ src/test/java/spaceinvaders/AlienTest.java
@@ -7,35 +7,37 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import spaceinvaders.model.AlienModel;
+import spaceinvaders.model.PositionModel;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AlienTest {
-    private Alien alien;
-    private Position position;
+    private AlienModel alien;
+    private PositionModel position;
 
     @BeforeEach
     public void helper(){
-    this.alien = new Alien(new Position(20, 9), '&');
-    this.position = new Position(20, 9);
+    this.alien = new AlienModel(new PositionModel(20, 9), '&');
+    this.position = new PositionModel(20, 9);
     }
 
     @Test
     public void draw() {
         TextGraphics graphics = Mockito.mock(TextGraphics.class);
-        alien.draw(graphics);
+        alien.getViewer().draw(graphics);
         Mockito.verify(graphics, Mockito.times(1)).setCharacter(new TerminalPosition(20, 9), TextCharacter.fromCharacter('&')[0]);
     }
 
     @Test
     public void move1(){
-        alien.move(new Position(1, 1));
+        alien.move(new PositionModel(1, 1));
         assertEquals(21,alien.getPosition().getX());
         assertEquals(10,alien.getPosition().getY());
     }
     @Test
     public void move2(){
-        alien.move(new Position(-1, -1));
+        alien.move(new PositionModel(-1, -1));
         assertEquals(19,alien.getPosition().getX());
         assertEquals(8,alien.getPosition().getY());
     }
