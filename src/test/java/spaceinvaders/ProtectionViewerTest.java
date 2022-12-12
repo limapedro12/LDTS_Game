362bf91aafd104ed3640875bbce3363d4144e2a9
up202108738 <up202108738@up.pt>
2022-12-12 12:28:46 +0000

    Protection model and viewer

--- src/test/java/spaceinvaders/ProtectionViewerTest.java
+++ src/test/java/spaceinvaders/ProtectionViewerTest.java
@@ -12,12 +12,13 @@ import spaceinvaders.model.ProtectionModel;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ProtectionTest {
+public class ProtectionViewerTest {
     ProtectionModel protection;
     @BeforeEach
     public void helper() {
         protection = new ProtectionModel(new PositionModel(2, 2), 2);
     }
+
     @Test
 
     public void draw() {
@@ -30,36 +31,5 @@ public class ProtectionTest {
     @Test
     public void getModelTest() {
         assertEquals(protection, protection.getViewer().getModel());
-    }
-
-    @Test
-    public void getWidth() {
-        assertEquals(4, protection.getWidth());
-    }
-
-    @Test
-    public void getHeight() {
-        assertEquals(2, protection.getHeight());
-    }
-
-    @Test
-    public void damage() {
-        protection.damage();
-        assertEquals(1, protection.getLife());
-    }
-
-    @Test
-    public void isAlive() {
-        assertEquals(true, protection.isAlive());
-        protection.damage();
-        assertEquals(true, protection.isAlive());
-        protection.damage();
-        assertEquals(false, protection.isAlive());
-    }
-
-    @Test
-    public void kill() {
-        protection.kill();
-        assertEquals(false, protection.isAlive());
     }
 }
